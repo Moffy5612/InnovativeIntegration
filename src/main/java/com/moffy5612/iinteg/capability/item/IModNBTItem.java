ee87c4545e4772bd002f8444337d36f592c9a7db
moffy5612 <a-eihave@outlook.jp>
2023-05-23 09:59:59 +0900

    #

--- src/main/java/com/moffy5612/iinteg/capability/item/IModNBTItem.java
+++
@@ -1,7 +0,0 @@
-package com.moffy5612.iinteg.capability.item;
-
-import com.moffy5612.iinteg.capability.ModCapabilityProvider;
-
-public interface IModNBTItem {
-    public ModCapabilityProvider getCapabilityProvider();
-}
