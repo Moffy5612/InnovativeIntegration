a444bbc00d35667b81ad6637749ce60bfd10fc3f
moffy5612 <a-eihave@outlook.jp>
2023-05-23 09:57:05 +0900

    #

--- src/main/java/com/moffy5612/iinteg/integration/tconstruct/TConstructIntegration.java
+++ src/main/java/com/moffy5612/iinteg/integration/tconstruct/TConstructIntegration.java
@@ -1,14 +1,18 @@
 package com.moffy5612.iinteg.integration.tconstruct;
 
 import com.moffy5612.iinteg.integration.tconstruct.book.conarm.ArmorModifierSectionTransformer;
+import com.moffy5612.iinteg.integration.tconstruct.event.TConstructCapabilityEvent;
 import com.moffy5612.iinteg.integration.tconstruct.handler.ModTraitHandler;
 import com.moffy5612.iinteg.misc.util.AnnotationUtil;
 
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Loader;
 
 public class TConstructIntegration{
     public static void preInit(){
         if(!Loader.isModLoaded("tconstruct"))return;
+
+        MinecraftForge.EVENT_BUS.register(new TConstructCapabilityEvent());
         
         AnnotationUtil.initHandler(ModTraitHandler.class);
     }
--- src/main/java/com/moffy5612/iinteg/integration/tconstruct/event/TConstructCapabilityEvent.java
+++ src/main/java/com/moffy5612/iinteg/integration/tconstruct/event/TConstructCapabilityEvent.java
@@ -10,13 +10,13 @@ import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import slimeknights.tconstruct.library.tinkering.ITinkerable;
 
-public class CapabilityEvent {
+public class TConstructCapabilityEvent {
     private static final ResourceLocation TINKERS_SOCKETABLE_TOOL = new ResourceLocation(Reference.MOD_ID, "iintegtinkerssocketable");
 
     @SubscribeEvent
     public void attachItemCapabilities(AttachCapabilitiesEvent<ItemStack> event) {
         if (event.getObject().getItem() instanceof ITinkerable) {
-            if(Loader.isModLoaded("conarm") && Loader.isModLoaded("psi"))
+            if(Loader.isModLoaded("psi"))
                 event.addCapability(TINKERS_SOCKETABLE_TOOL, new TinkersArmorCapability(event.getObject()));
         }
     }
